f75f7991f6428c3733c59bce3f39a806c31aff63
huketo <fonytailmoe@gmail.com>
2025-09-14 02:49:16 +0900

    PointCalculator.sol에서 주석 제거 및 PointEmitter.sol에서 불필요한 주석 삭제

--- 2-1_Solidity_핵심/contracts/PointCalculator.sol
+++ 2-1_Solidity_핵심/contracts/PointCalculator.sol
@@ -6,7 +6,6 @@ library PointCalculator {
     uint256 private constant DENOMINATOR = 100;
 
     function getPoints(uint256 ethAmount) internal pure returns (uint256) {
-        // 정밀도 손실 방지를 위해 곱셈을 먼저 수행합니다.
         return (ethAmount * NUMERATOR) / DENOMINATOR;
     }
 }
--- 2-1_Solidity_핵심/contracts/PointEmitter.sol
+++ 2-1_Solidity_핵심/contracts/PointEmitter.sol
@@ -32,16 +32,12 @@ contract PointEmitter {
     }
 
     function withdraw() public view onlyOwner {
-        // 1. 인출할 금액 (컨트랙트의 전체 잔액)
         uint256 amount = address(this).balance;
 
-        // 2. 소유자에게 ETH 전송 (call 메소드 사용)
-        // 전송 실패 시 트랜잭션을 되돌리기 위해 require 구문으로 감쌉니다.
         (bool success, ) = i_owner.call{value: amount}("");
         require(success, "Failed to send Ether");
     }
 
-    // 이 컨트랙트에 ETH가 직접 보내지면 contribute 함수를 호출합니다.
     receive() external payable {
         contribute();
     }
